de5b256aac50becdebea1c16ef5aa39168dde0b7
Vanchenko <vanchenkoev@pakline.com.ua>
2023-09-23 22:15:25 +0300

    метод add user's recipe тепер добавляє зображення коктейлю

--- controllers/drinks/addRecipe.js
+++ controllers/drinks/addRecipe.js
@@ -2,13 +2,18 @@ const { recipesModel, schema } = require('../../models/recipesModel');
 const  HttpError = require('../../helpers/HttpError');
 
 const addRecipe = async (req, res) => {
-    const { error } = schema.validate(req.body);
+    if (!req.file || !req.file.path) {
+        throw HttpError(400, "File is required for this request");
+      }
+        const recipeAdd = {...req.body, drinkThumb: req.file.path, ingredients: JSON.parse(req.body.ingredients) };
+        console.log('Recipe ADD', recipeAdd);
+    const { error } = schema.validate(recipeAdd);
     if (error) {
         res.status(400);
         throw HttpError(400, 'Provide all fields data');
     }
     const { _id } = req.user;
-    const result = await recipesModel.create({ ...req.body, owner: _id });
+    const result = await recipesModel.create({ ...recipeAdd, owner: _id });
     res.status(201).json({
         code: 201,
         message: 'success',
