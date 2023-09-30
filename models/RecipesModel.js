3fcd8d940ec8a34995eeca7f3dfe05f5389c7283
MolchanovSergii <molchanse@gmail.com>
2023-09-30 15:41:54 +0300

    summary

--- models/RecipesModel.js
+++ models/RecipesModel.js
@@ -2,11 +2,11 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const errorMongooseHandler = require("../helpers/errorMongooseHandler");
 
-const alcoholList = ['Alcoholic','Non alcoholic','Optional alcohol'];
-const categoriesList = ["Beer", "Cocktail", "Cocoa", "Coffee / Tea", "Coffee/Tea", "Homemade Liqueur",
-  "Ordinary Drink", "Other/Unknown", "Punch / Party Drink", "Shake", "Shot", "Soft Drink"];
-const glassList = ["Beer", "Cocktail", "Cocoa", "Coffee / Tea", "Coffee/Tea", "Homemade Liqueur", "Ordinary Drink",
-  "Other/Unknown", "Punch / Party Drink", "Shake", "Shot", "Soft Drink"];
+// const alcoholList = ['Alcoholic','Non alcoholic','Optional alcohol'];
+// const categoriesList = ["Beer", "Cocktail", "Cocoa", "Coffee / Tea", "Coffee/Tea", "Homemade Liqueur",
+//   "Ordinary Drink", "Other/Unknown", "Punch / Party Drink", "Shake", "Shot", "Soft Drink"];
+// const glassList = ["Beer", "Cocktail", "Cocoa", "Coffee / Tea", "Coffee/Tea", "Homemade Liqueur", "Ordinary Drink",
+//   "Other/Unknown", "Punch / Party Drink", "Shake", "Shot", "Soft Drink"];
 
 const recipesSchema = new Schema(
   {
@@ -66,16 +66,16 @@ const recipesSchema = new Schema(
     },
     ingredients: {
       type: Array,
-          title:{
-            type: String,
-          },
-          measure:{
-            type: String,
-          },
-          ingredientId: {
-            type: Schema.Types.ObjectId,
-            ref: "ingredient",
-          },
+      title: {
+        type: String,
+      },
+      measure: {
+        type: String,
+      },
+      ingredientId: {
+        type: Schema.Types.ObjectId,
+        ref: "ingredient",
+      },
     },
     favorites: {
       type: Array,
@@ -101,41 +101,41 @@ recipesSchema.post("save", errorMongooseHandler);
 
 const schemaAddRecipe = Joi.object({
   drink: Joi.string().min(1).required().messages({
-    'string.base' : 'Drink should be a type of string',
-    'string.empty' : 'The drink field isnt be empty',
-    'string.min': 'Drink should have a minimum length of {#limit} characters.',
-    'any.required': 'Drink is a required field.',
+    "string.base": "Drink should be a type of string",
+    "string.empty": "The drink field isnt be empty",
+    "string.min": "Drink should have a minimum length of {#limit} characters.",
+    "any.required": "Drink is a required field.",
   }),
   tags: Joi.string().messages({
-    'string.base' : 'Tags should be a type of string',
+    "string.base": "Tags should be a type of string",
   }),
-  category: Joi.string().valid(...categoriesList).required().messages({
-    'string.base' : 'Category should be a type of string',
-    'any.required': 'Category is a required field.'
+  category: Joi.string().required().messages({
+    "string.base": "Category should be a type of string",
+    // 'any.required': 'Category is a required field.'
   }),
-  alcoholic: Joi.string().valid(...alcoholList).required().messages({
-    'string.base' : 'Alcoholic type should be a type of string',
-    'any.required': 'Alcoholic type is a required field.'
+  alcoholic: Joi.string().required().messages({
+    "string.base": "Alcoholic type should be a type of string",
+    // 'any.required': 'Alcoholic type is a required field.'
   }),
-  glass: Joi.string().valid(...glassList).required().messages({
-    'string.base' : 'Glass should be a type of string',
-    'any.required': 'Glass is a required field.'
+  glass: Joi.string().required().messages({
+    "string.base": "Glass should be a type of string",
+    // 'any.required': 'Glass is a required field.'
   }),
   description: Joi.string().required().messages({
-    'string.base' : 'Description should be a type of string',
-    'any.required': 'Description is a required field.'
+    "string.base": "Description should be a type of string",
+    "any.required": "Description is a required field.",
   }),
   instructions: Joi.string().required().messages({
-    'string.base' : 'Instructions should be a type of string',
-    'any.required': 'Instructions is a required field.'
+    "string.base": "Instructions should be a type of string",
+    "any.required": "Instructions is a required field.",
   }),
   instructionsUK: Joi.string().messages({
-    'string.base' : 'InstructionsUK should be a type of string',
+    "string.base": "InstructionsUK should be a type of string",
   }),
   drinkThumb: Joi.string(),
   ingredients: Joi.string(),
   shortDescription: Joi.string().messages({
-    'string.base' : 'Short description should be a type of string',
+    "string.base": "Short description should be a type of string",
   }),
 });
 
